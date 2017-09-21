297cbdbd5f9d49bc3736ba3fe748e8a49e0484eb
Münteha <munteha.yildiz@mobilityhouse.com>
2017-09-21 10:18:38 +0200

    Add numerical and face values
    Add these for each card

--- blackjack/__init__.py
+++ blackjack/__init__.py
@@ -1,10 +1,14 @@
 class Card:
 
-    def __init__(self, suit):
+    def __init__(self, suit, value):
         if suit not in ['spade', 'diamond', 'club', 'heart']:
             raise ValueError(
                 "{} must be in ['spade', 'diamond', 'club', 'heart']")
         self.suit = suit
+
+        if value not in ['A', 'Q', 'K', 'J'] and value not in list(range(2, 11)):
+            raise ValueError
+        self.value = value
 
 
 class Deck:
